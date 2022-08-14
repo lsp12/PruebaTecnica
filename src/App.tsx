1bd2db3dc4e0dd6eba5fa7fdb4841cc72fdcb463
lsp12 <jonathankenny852@gmail.com>
2022-08-13 23:52:12 -0500

    added change theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { createTheme, CssBaseline, PaletteMode, ThemeProvider } from '@mui/material'
+import { useAppSelector } from './app/hooks'
 import { Home } from './modules/home/pages/Home'
 
 function App() {
-  return <Home />
+  const { theme } = useAppSelector((state) => state.ui)
+  const darkTheme = createTheme({
+    palette: {
+      mode: (theme as PaletteMode) || 'light',
+    },
+  })
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Home />
+      <CssBaseline />
+    </ThemeProvider>
+  )
 }
 
 export default App
--- src/modules/home/pages/Home.tsx
+++ src/modules/home/pages/Home.tsx
@@ -1,18 +1,31 @@
-import { Container, Grid } from '@mui/material'
+import { Box, Button, Container, Grid } from '@mui/material'
 import React, { useEffect } from 'react'
-import { useAppDispatch } from '../../../app/hooks'
+import { useAppDispatch, useAppSelector } from '../../../app/hooks'
+import { setTheme } from '../../shared/slice/ui.slice'
 import { apiPoke } from '../actions/home.action'
 import { LeftComponents } from '../components/leftComponent/Index'
 import { RightComponent } from '../components/rightComponent/Index'
+import Sol from '@mui/icons-material/Brightness7'
+import Luna from '@mui/icons-material/Brightness4'
 
 export const Home = () => {
+  const { theme } = useAppSelector((state) => state.ui)
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch(apiPoke())
   }, [dispatch])
   return (
     <Container>
-      <Grid container spacing={2}>
+      <Box>
+        <Button
+          onClick={() => {
+            dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
+          }}
+        >
+          {theme === 'light' ? <Sol /> : <Luna />} Change theme
+        </Button>
+      </Box>
+      <Grid container spacing={2} pt={5}>
         <Grid item xs={12} md={6}>
           <LeftComponents />
         </Grid>
